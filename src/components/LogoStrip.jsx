982a8fa42ad97de56755da27d3364011081824e2
suraj-kumar89 <suraj@bluvodigital.com>
2026-03-05 23:30:28 +0530

    Change logo

--- src/components/LogoStrip.jsx
+++ src/components/LogoStrip.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./LogoStrip.css";
 
-import LeftLogo from "../assets/images/Vector.png";   // your first PNG
+// import LeftLogo from "../assets/images/Vector.png";   // your first PNG
 import RightLogo from "../assets/images/logo.png"; // your second PNG
-import CrossIcon from "../assets/images/x.svg"; // OR you can use plain "×" text
+// import CrossIcon from "../assets/images/x.svg"; // OR you can use plain "×" text
 
 export default function LogoStrip() {
   return (
     <div className="logo-strip">
       {/* LEFT LOGO */}
-      <img src={LeftLogo} alt="Left Logo" className="logo-img" />
+      {/* <img src={LeftLogo} alt="Left Logo" className="logo-img" /> */}
 
       {/* CENTER X */}
       {/* If using SVG:  */}
-      <img src={CrossIcon} alt="x" className="cross-icon" />
+      {/* <img src={CrossIcon} alt="x" className="cross-icon" /> */}
 
       {/* If you want a TEXT X instead, replace above with:
       <span className="cross-text">×</span>
